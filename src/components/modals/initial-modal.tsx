92cfaadf72c7672302bc64b8fa47bc9f84c5d86f
Geraldo Luiz <geraldobl58@gmail.com>
2024-03-06 16:05:23 -0300

    feat: Add modal component

--- src/components/modals/initial-modal.tsx
+++ src/components/modals/initial-modal.tsx
@@ -3,6 +3,10 @@
 import { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 
+import { useRouter } from "next/navigation";
+
+import axios from "axios";
+
 import * as z from "zod";
 
 import { zodResolver } from "@hookform/resolvers/zod";
@@ -25,7 +29,7 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
-import { FileUpload } from "../file-upload";
+import { FileUpload } from "@/components/file-upload";
 
 const formSchema = z.object({
   name: z.string().min(1, {
@@ -39,6 +43,8 @@ const formSchema = z.object({
 export const InitialModal = () => {
   const [isMounted, setIsMounted] = useState(false);
 
+  const router = useRouter();
+
   const form = useForm({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -50,7 +56,15 @@ export const InitialModal = () => {
   const isLoading = form.formState.isSubmitting;
 
   const onSubmit = async (values: z.infer<typeof formSchema>) => {
-    console.log(values);
+    try {
+      await axios.post("/api/servers", values);
+
+      form.reset();
+      router.refresh();
+      window.location.reload();
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   useEffect(() => {
