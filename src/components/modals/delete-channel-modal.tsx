aa04bddfadda74522df7e692a07ea98fa048f821
Geraldo Luiz <geraldobl58@gmail.com>
2024-03-11 14:26:34 -0300

    feat: Add delete channel modal component

---
+++ src/components/modals/delete-channel-modal.tsx
@@ -0,0 +1,86 @@
+"use client";
+
+import { useState } from "react";
+
+import { useParams, useRouter } from "next/navigation";
+
+import axios from "axios";
+
+import qs from "query-string";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+
+import { Button } from "@/components/ui/button";
+
+import { useModal } from "@/hooks/use-modal";
+
+export const DeleteChannelModal = () => {
+  const { isOpen, onClose, type, data } = useModal();
+
+  const router = useRouter();
+  const params = useParams();
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  const isModalOpen = isOpen && type === "deleteChannel";
+
+  const { server, channel } = data;
+
+  const onClick = async () => {
+    try {
+      setIsLoading(true);
+
+      const url = qs.stringifyUrl({
+        url: `/api/channels/${channel?.id}`,
+        query: {
+          serverId: server?.id,
+        },
+      });
+
+      await axios.delete(url);
+
+      onClose();
+      router.refresh();
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    <Dialog open={isModalOpen} onOpenChange={onClose}>
+      <DialogContent className="bg-white text-black p-0 overflow-hidden">
+        <DialogHeader className="pt-8 px-6">
+          <DialogTitle className="text-2xl text-center font-bold">
+            Delete Channel
+          </DialogTitle>
+          <DialogDescription className="text-center text-zinc-500">
+            Are you sure you want to do this? <br />
+            <span className="font-semibold text-indigo-500">
+              #{channel?.name}
+            </span>{" "}
+            will br permanently deleted?
+          </DialogDescription>
+        </DialogHeader>
+        <DialogFooter className="bg-gray-100 px-6 py-4">
+          <div className="flex items-start justify-between w-full">
+            <Button disabled={isLoading} onClick={onClose}>
+              Cancel
+            </Button>
+            <Button variant="primary" disabled={isLoading} onClick={onClick}>
+              Confirm
+            </Button>
+          </div>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+};
